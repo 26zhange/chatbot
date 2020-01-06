027edaf8d7097427572143eb72d77e7cda7cf6e5
26zhange <58191829+26zhange@users.noreply.github.com>
2020-01-05 18:13:23 -0600

    Update chatbot 1.0.py

--- chatbot 1.0.py
+++ chatbot 1.0.py
@@ -1,33 +1,71 @@
-import io
-import random
+import time
 import string
-import warnings
-import numpy as np
-import spacy
-import warnings
-import nltk
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-nltk.download('popular', quiet=True) 
-nltk.download('punkt') 
-nltk.download('wordnet')
-def chat():
+import math
+import random
+class main():
     print("Hi, this is Chatbot 1.0")
-    
-    f=open('chatbot.txt','r',errors = 'ignore')
-    raw=f.read()
-    raw = raw.lower()# converts to lowercase
-    user_input = input("user:")
     bot_response = ""
+
+    #dictionaries and array for conversation words
     responses = {
-        "hi":"hi",
-        "hello":"hello",
-        "what's your name"or "what is your name":"My name is chatbot"
+        "hi":"hi,tell me a little bit about yourself",
+        "when where you born" : "I forgot",
+        "hello":"hello, tell me a little bit about yourself",
+        "what is your name":"My name is chatbot",
+        "how are you":"I am well",
+        "what is your favorite color":"My favorite color is blue",
+        "do you like" : "Yes" or "no"
+
 
         }
-    if '?' or '.' in user_input:
-        string.remove('?')
-        string.remove('.')
-    if user_input in responses:
-        print("bot:"+ responses[user_input])
- 
+    bot_question = ["What's your name?","How are you?","What's your favorite color","How old are you?"]
+    numbers = {
+        "one": 1,
+        "two":2,
+        "three":3
+        }
+
+    #fuctions of user input
+    what_user_likes = []
+    user_input = ""
+    while  user_input != "bye":
+        user_input = input("user:")
+        message = user_input.lower()
+        if '?' or '.' in user_input:
+            message = message.replace('?',"")
+            message = message.replace('.',"")
+        if "what's" in user_input:
+        
+            
+            time.sleep(1)
+            message = message.replace("what's", "what is")
+        if message in responses:
+            time.sleep(1)
+            print("bot:"+ responses[message])
+        elif "do you like" in message:
+            time.sleep(1)
+            message = "do you like"
+            print("bot:" + responses[message])
+        elif message == "bye":
+            time.sleep(1)
+            print("bot: bye")
+            break
+
+        elif "my name is " in message:
+            time.sleep(1)
+            message = message.replace("my name is","")
+            name = message
+            print("bot: Cool name"+name+",tell me more")
+        elif "I am" and "years old" in message:
+            message = message.replace("I am"and"years old", "")
+            age = message
+            print("bot: tell me more")
+        elif "How are you?" in bot_question and "i am" in message:
+            message = message.replace("i am","")
+            user_emotion = message
+        elif "i like" in message:
+            message = message.replace("i like","")
+            what_user_likes = what_user_likes.append(message)
+            print("bot: I also like"+message)
+        else:
+            print("bot:"+ random.choice(bot_question) )
